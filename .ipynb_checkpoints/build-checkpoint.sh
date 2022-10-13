2803cc680d311fb1e39c19c44ecea30b124afe3f
340a8 <1652993@tongji.edu.cn>
2022-10-13 15:06:54 +0800

    Update README.md

--- .ipynb_checkpoints/build-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-# install preprocessing
-cd /fs/home/weihailin/SCREE/pre-process/
-python setup.py install
-
-# install R packages
-Rscript /fs/home/weihailin/SCREE/install.R
--- .ipynb_checkpoints/install-checkpoint.R
+++
@@ -1,2 +0,0 @@
-library(devtools)
-install_github("https://github.com/HailinWei98/SCREE/")
