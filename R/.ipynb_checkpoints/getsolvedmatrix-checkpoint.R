a9bbfbb9c2a3ebfc03fe14cfc54415480b384233
HailinWei98 <46320456+HailinWei98@users.noreply.github.com>
2023-03-09 12:24:32 +0800

    Delete R/.ipynb_checkpoints directory

--- R/.ipynb_checkpoints/getsolvedmatrix-checkpoint.R
+++
@@ -1,9 +0,0 @@
-#' function definitions ##### Return liner regression results of perturbation score
-
-getsolvedmatrix <- function(Xm, Ym, lambda = 0.01) {
-    # Amat=solve(Xmat,Ymat) # solve AX=B, or Xmat * A =Ymat
-    TMmat_g = crossprod(Xm, Xm) + lambda * diag(ncol(Xm))
-
-    Amat_g = Ym %*% Xm %*% solve(TMmat_g)
-    return(Amat_g)
-}
